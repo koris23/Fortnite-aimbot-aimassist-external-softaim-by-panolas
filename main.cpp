c0ad3bba94ab8515dce9f51ac64203eb69ee6d16
Panolas <105129035+PanolasFN@users.noreply.github.com>
2023-02-28 01:37:41 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "https://discord.gg/aimassist\m";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\m";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\m";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    std::cout << "https://discord.gg/aimassist\n";
+    return 0;
+}
